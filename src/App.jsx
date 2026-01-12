672d86814df2137fd2d5b8246d547b3b34ff9e17
chatcha1234 <chatchawan.da1@gmail.com>
2026-01-12 18:39:40 +0700

    feat: restructure project, add AI design page, footer, and UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './views/Home';
-import Login from './views/Login';
-import Register from './views/Register';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import AiDesign from "./views/AiDesign";
 
 const App = () => {
   return (
@@ -12,10 +13,10 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="Login" element={<Login />} />
         <Route path="Register" element={<Register />} />
-        
+        <Route path="design/:productId" element={<AiDesign />} />
       </Route>
     </Routes>
   );
-}
+};
 
-export default App
+export default App;
